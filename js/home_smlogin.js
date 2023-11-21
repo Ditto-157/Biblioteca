0bec0c0ad1b7192bed8f712b687d140078d76e35
Felifelps <felipefelipe23456@gmail.com>
2023-11-21 07:13:51 -0300

    Index updated

--- js/home_smlogin.js
+++ js/home_smlogin.js
@@ -1,146 +1,219 @@
-document.cookie = "nomeDoCookie=valor; secure; HttpOnly";
 const pesquisarLivros = document.getElementById('pesquisa');
 const livrosRows = document.getElementById('table-livros');
-const url = 'https://apibiblioteca.2.ie-1.fl0.io/books';
-const data = { key: 'f1563cb61eaf857ce3042c12cd94e774'};
-const tableBody = document.getElementById("table").getElementsByTagName("tbody")[0]
-const itemsPerPage = 24;
+const url = 'https://apibiblioteca.2.ie-1.fl0.io/';
+const baseData = { key: 'f1563cb61eaf857ce3042c12cd94e774' };
+const tableBody = document.getElementById("table-livros");
+const flash = document.getElementById('flash-message');
+var pages = [];
+var current_page = null;
 
+pesquisarLivros.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        searchByTitle();
+    }
+});
 
-function search() {
-        const termoPesquisa = pesquisarLivros.value.trim().toLowerCase();
-        
-        if (termoPesquisa === '') {
-            return changeTablePage(1);
+function queryBook(query) {
+    removeAllRows();
+    query['key'] = baseData.key;
+    fetch(url + "books/search", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(query)
+    })
+    .then((response) => response.json())
+    .then(data => {
+        let keys = Object.keys(query);
+        let ids = Object.keys(data);
+        let livros = Object.values(data).sort((book) => {
+            
+            let search = query[keys[0]].toLowerCase();
+            let bookValue = book[keys[0]].toLowerCase();
+            return bookValue === search ? 1 : -1;
+        }).reverse();
+        for (let i = 0; i < livros.length; i++) {
+            insertRow(ids[i], livros[i]);
         }
-    
-        for (let i = 0; i < livrosRows.children.length; i++) {
-            row = livrosRows.getElementsByTagName('tr').item(i);
-            const titulo = row.children.item(0).textContent.toLowerCase();
-    
-            const correspondeTermo = titulo.includes(termoPesquisa);
-    
-            if (correspondeTermo) {
-                row.style.display = 'table-row';
-            } else {
-                row.style.display = 'none';
-            }
-        };
+    });
 }
 
-pesquisarLivros.addEventListener('input', search);
-document.getElementById('btn-pesquisar').addEventListener('click', search);
+function showFlash(text) {
+    flash.textContent = text;
+    flash.classList.remove('d-none');
+}
+
+function hideFlash() {
+    flash.classList.add('d-none');
+}
+
+function removeAllRows() {
+    for (let i = 0; i < tableBody.children.length; i ++) {
+        let item = tableBody.children.item(i);
+        item.style.display = 'none';
+    };
+}
+
+function insertRow(id, livro) {
+    var row = tableBody.insertRow();
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = livro.titulo;
+    cell2.innerHTML = livro.autor;
+    cell3.innerHTML = livro.assuntos;
+    cell4.innerHTML = `
+    <a href='/paginas/login.html'><button class='btn btn-success'> Solicitar empréstimo </button></a>
+    `;
+}
+
+function searchByTitle() {
+    const termoPesquisa = pesquisarLivros.value.trim().toLowerCase();
+
+    if (termoPesquisa === '') {
+        return changeTablePage(1);
+    }
+
+    queryBook({
+        titulo: termoPesquisa
+    });
+}
 
 function changeTablePage(page) {
-    var startIndex = (page - 1) * itemsPerPage;
-    var endIndex = startIndex + itemsPerPage;
-
-    for (let i = 0; i < tableBody.children.length; i ++) {
-        row = tableBody.children.item(i);
-        if (i >= startIndex && i < endIndex) {
-            row.style.display = 'table-row';
-        } else {
-            row.style.display = 'none';
+    removeAllRows();
+    var page = page;
+    if (pages.includes(page)) {
+        let start = ((page - 1) * 24);
+        for (let i = 0; i < tableBody.children.length; i ++) {
+            let item = tableBody.children.item(i);
+            if (i < start + 24 && i >= start ) {
+                item.style.display = 'table-row';
+            }
         }
-    }
-}
-
-function setupPagination(livros) {
-    var totalItems = livros.length;
-    var totalPages = Math.ceil(totalItems / itemsPerPage);
-    var pagination = document.getElementById("pagination");
-    pagination.innerHTML = "";
-
-    for (var i = 1; i <= totalPages; i++) {
-        var li = document.createElement("li");
-        var a = document.createElement("a");
-        li.classList.add("page-item");
-        a.classList.add("page-link");
-        a.href = "#";
-        a.innerHTML = i;
-
-        a.onclick = (function(page){
-            return function() {
-                changeTablePage(page, livros);
-            };
-        })(i);
-        li.appendChild(a);
-        pagination.appendChild(li);
-    }
-}
-
-function preencherDrop(dropdownId, dados) {
-    const dropdown = document.getElementById(dropdownId);
-
-    var menu = dropdown.querySelector('.dropdown-menu');
-    menu.innerHTML = '';
-
-    const todosItem = document.createElement('li');
-    todosItem.classList.add('dropdown-item');
-    todosItem.dataset.value = '';
-    todosItem.textContent = `Todos os ${dropdownId === 'autorDrop' ? 'autores' : 'assuntos'}`;
-    todosItem.addEventListener('click', () => {changeTablePage(1);})
-    menu.appendChild(todosItem);
-    
-    dados.forEach(dado => {
-        const item = document.createElement('li');
-        item.classList.add('dropdown-item');
-        item.dataset.value = dado;
-        item.textContent = dado;
-        item.addEventListener('click', () => {
-            for (let i = 0; i < livrosRows.children.length; i++) {
-                row = livrosRows.getElementsByTagName('tr').item(i);
-                let autor = row.children.item(1).textContent;
-                let assuntos = row.children.item(2).textContent;
-                
-                let correspondeTermo = false;
-                if (dropdownId === 'autorDrop') {
-                    correspondeTermo = autor === dado;
-                } else {
-                    correspondeTermo = assuntos === dado;
-                }
-        
-                if (correspondeTermo) {
-                    row.style.display = 'table-row';
-                } else {
-                    row.style.display = 'none';
-                }
-            };
+    } else {
+        fetch(url + "books/page", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                key: baseData.key,
+                page: page
+            })
         })
-
-        dropdown.querySelector('.dropdown-menu').appendChild(item);
-    });
-
-}
-fetch(url, {
-    method: "POST",
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data)
-})
-
-.then((response) => response.json())
-
-.then(data => {
-    const livros = Object.values(data);
-    for (let i = 0; i < livros.length; i ++){
-        var row = tableBody.insertRow();
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3); 
-        cell1.innerHTML = livros[i].titulo;
-        cell2.innerHTML = livros[i].autor;
-        cell3.innerHTML = livros[i].assuntos;
-        cell4.innerHTML = "<a href='login.html'><button class='btn btn-success'> Solicitar Empréstimo </button></a>";
+        .then((response) => response.json())
+        .then(data => {
+            let ids = Object.keys(data);
+            let livros = Object.values(data);
+            console.log(livros);
+            for (let i = 0; i < livros.length; i++) {
+                insertRow(ids[i], livros[i]);
+            }
+            pages.push(page);
+        });
     }
-    changeTablePage(1);
-    setupPagination(livros);
+}
 
-    let autores = livros.map((livro) => livro.autor);
-    let assuntos = livros.map((livro) => livro.assuntos);
-    preencherDrop('autorDrop', autores);
-    preencherDrop('assuntoDrop', assuntos);
-    tableBody.dispatchEvent(new FocusEvent('focus'));
-});
+function setupPagination() {
+    fetch(url + "books/length", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(baseData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        totalItems = data.len;
+        var totalPages = Math.ceil(totalItems / 24);
+        var pagination = document.getElementById("pagination");
+        pagination.innerHTML = "";
+
+        for (var i = 1; i <= totalPages; i++) {
+            var li = document.createElement("li");
+            var a = document.createElement("a");
+            li.classList.add("page-item");
+            a.classList.add("page-link");
+            a.innerHTML = i;
+
+            a.onclick = (function (page) {
+                return function () {
+                    changeTablePage(page);
+                };
+            })(i);
+            li.appendChild(a);
+            pagination.appendChild(li);
+        }
+    });
+}
+
+function preencherDrop(drop_id) {
+    var dropField = null;
+    let all_text = null;
+    switch (drop_id) {
+        case 'autorDrop':
+            dropField = 'autor';
+            all_text = 'os autores';
+            break;
+        case 'assuntoDrop':
+            dropField = 'assuntos';
+            all_text = 'os assuntos';
+            break;
+        default: 
+            break;
+    }
+
+    fetch(url + "books/field_values", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            key: baseData.key,
+            field: dropField
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        const dropdown = document.getElementById(drop_id);
+
+        var menu = dropdown.querySelector('.dropdown-menu');
+        menu.innerHTML = '';
+
+        const todosItem = document.createElement('li');
+        todosItem.classList.add('dropdown-item');
+        todosItem.dataset.value = '';
+        todosItem.textContent = 'Todos ' + all_text;
+        todosItem.addEventListener('click', () => {changeTablePage(1);});
+        menu.appendChild(todosItem);
+
+        let looked = [];
+
+        data.values.forEach(value => {
+            if (looked.includes(value)) {
+                return;
+            }
+            looked.push(value);
+            const item = document.createElement('li');
+            item.classList.add('dropdown-item');
+            item.dataset.value = value;
+            item.textContent = value;
+            let query = {}
+            query[dropField] = value;
+            item.addEventListener('click', () => {
+                queryBook(query);
+            })
+
+            dropdown.querySelector('.dropdown-menu').appendChild(item);
+        });
+    });
+}
+
+
+changeTablePage(1);
+setupPagination();
+preencherDrop('autorDrop');
+preencherDrop('assuntoDrop');
+
