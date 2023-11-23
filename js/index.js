f36d391aad553b22080a9ac3acd9c3e5d8c757b8
Felifelps <felipefelipe23456@gmail.com>
2023-11-23 08:44:42 -0300

    removed all consoles

--- js/index.js
+++ js/index.js
@@ -64,12 +64,12 @@ function insertRow(id, livro) {
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
     cell1.innerHTML = livro.titulo;
     cell2.innerHTML = livro.autor;
     cell3.innerHTML = livro.assuntos;
-    cell4.innerHTML = `
-    <a href='/paginas/login.html'><button class='btn btn-success'> Solicitar empréstimo </button></a>
-    `;
+    cell4.innerHTML = livro.edicao;
+    cell5.innerHTML = livro.editora;
 }
 
 function searchByTitle() {
