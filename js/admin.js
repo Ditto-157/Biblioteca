45adcf56e291e2afe2dfbd2674a13cda4698d197
Felifelps <felipefelipe23456@gmail.com>
2023-11-23 10:19:00 -0300

    Style changes

--- js/admin.js
+++ js/admin.js
@@ -1,41 +1,37 @@
-document.addEventListener("DOMContentLoaded",
-    function () {
+var loading = document.getElementById("loading");
+var container = document.getElementById("container");
 
-        var loading = document.getElementById("loading");
-        var container = document.getElementById("container");
+container.classList.add('invisible')
+loading.style.display = "flex";
 
-        container.classList.add('invisible')
-        loading.style.display = "flex";
-
-        function checkIfAdminIsLogged() {
-            if (!sessionStorage.getItem('token')) {
+function checkIfAdminIsLogged() {
+    if (!sessionStorage.getItem('token')) {
+        let lastPart = window.location.href.split('/');
+        lastPart = lastPart[lastPart.length - 1];
+        window.location.replace(window.location.href.replace(lastPart, 'admin/login'))
+    } else {
+        fetch('https://apibiblioteca.2.ie-1.fl0.io/admin/check', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                key: 'f1563cb61eaf857ce3042c12cd94e774',
+                token: sessionStorage.getItem('token')
+            })
+        })
+        .then((response) => response.json())
+    
+        .then(data => {
+            if (!data.message) {
                 let lastPart = window.location.href.split('/');
                 lastPart = lastPart[lastPart.length - 1];
                 window.location.replace(window.location.href.replace(lastPart, 'admin/login'))
             } else {
-                fetch('https://apibiblioteca.2.ie-1.fl0.io/admin/check', {
-                    method: "POST",
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        key: 'f1563cb61eaf857ce3042c12cd94e774',
-                        token: sessionStorage.getItem('token')
-                    })
-                })
-                .then((response) => response.json())
-            
-                .then(data => {
-                    if (!data.message) {
-                        let lastPart = window.location.href.split('/');
-                        lastPart = lastPart[lastPart.length - 1];
-                        window.location.replace(window.location.href.replace(lastPart, 'admin/login'))
-                    } else {
-                        container.classList.remove('invisible');
-                        loading.style.display = "none";
-                    }
-                });
+                container.classList.remove('invisible');
+                loading.style.display = "none";
             }
-        }
-        checkIfAdminIsLogged();
-    });
+        });
+    }
+}
+checkIfAdminIsLogged();
