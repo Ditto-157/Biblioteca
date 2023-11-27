a9a307f646d9d34a012f7827e29eb46db80b1595
Felifelps <felipefelipe23456@gmail.com>
2023-11-27 08:22:37 -0300

    Footer correction

--- js/index.js
+++ js/index.js
@@ -61,7 +61,7 @@ function removeAllRows() {
 }
 
 function insertRow(id, livro) {
-    if (footer.style.position === 'absolute' && window.innerHeight > document.body.clientHeight) {
+    if (footer.style.position === 'absolute' && (window.innerHeight || document.documentElement.clientHeight) > document.body.clientHeight) {
         footer.style.position = 'relative';
     };
     var row = tableBody.insertRow();
