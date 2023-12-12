bc3811d2f36273a13ab069693159b14547a7a635
Felifelps <felipefelipe23456@gmail.com>
2023-12-12 08:32:50 -0300

    Admin logic

--- js/admin.js
+++ js/admin.js
@@ -15,32 +15,34 @@ function checkIfAdminIsLogged() {
         let tokenDate = new Date(localStorage.getItem('tokenCheckDate'));
         console.log(today.getTime(), tokenDate.getTime(), today.getTime() <= tokenDate.getTime())
 
-        if (today.getTime() <= tokenDate.getTime()) {
+        if (tokenDate.getTime() < today.getTime()) {
+            fetch('https://bibliotecamilagres-503s.onrender.com/admin/check', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    token: localStorage.getItem('token')
+                })
+            })
+            .then((response) => response.json())
+        
+            .then(data => {
+                if (!data.message) {
+                    let lastPart = window.location.href.split('/');
+                    lastPart = lastPart[lastPart.length - 1];
+                    window.location.replace(window.location.href.replace(lastPart, 'admin/login'))
+                } else {
+                    container.classList.remove('invisible');
+                    loading.style.display = "none";
+                }
+            });
+        } else {
             container.classList.remove('invisible');
             loading.style.display = "none";
             return 0;
         }
-        fetch('https://bibliotecamilagres-503s.onrender.com/admin/check', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                token: localStorage.getItem('token')
-            })
-        })
-        .then((response) => response.json())
-    
-        .then(data => {
-            if (!data.message) {
-                let lastPart = window.location.href.split('/');
-                lastPart = lastPart[lastPart.length - 1];
-                window.location.replace(window.location.href.replace(lastPart, 'admin/login'))
-            } else {
-                container.classList.remove('invisible');
-                loading.style.display = "none";
-            }
-        });
+        
     }
 }
 checkIfAdminIsLogged();
