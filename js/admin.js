6afc266f117d1c689b7b5cd997d03b04bf60e952
Felifelps <felipefelipe23456@gmail.com>
2023-12-12 08:26:54 -0300

    Admin logic

--- js/admin.js
+++ js/admin.js
@@ -6,15 +6,15 @@ container.classList.add('invisible')
 loading.style.display = "flex";
 
 function checkIfAdminIsLogged() {
-    console.log(!localStorage.getItem('token'))
-    console.log(!localStorage.getItem('tokenCheckDate'))
+    console.log(localStorage.getItem('token'), !localStorage.getItem('token'))
+    console.log(localStorage.getItem('tokenCheckDate'))
     if (!localStorage.getItem('token')) {
         let lastPart = window.location.href.split('/');
         lastPart = lastPart[lastPart.length - 1];
         window.location.replace(window.location.href.replace(lastPart, 'admin/login'))
     } else {
         let today = new Date();
-        if (today.getDate() >= (new Date(localStorage.getItem('tokenCheckDate'))).getDate()) {
+        if (today.getDate().getTime() <= (new Date(localStorage.getItem('tokenCheckDate'))).getDate().getTime()) {
             container.classList.remove('invisible');
             loading.style.display = "none";
             return 0;
